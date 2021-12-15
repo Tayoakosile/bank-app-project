66a5f006f1089ded007b54d31f6011a50e929eca
Tayo Akosile <oluwatayoakosile@gmail.com>
2021-12-15 13:44:13 +0100

    update:Created transfer to options

--- client/src/components/Back.js
+++ client/src/components/Back.js
@@ -1,38 +1,39 @@
-import { IconButton } from '@chakra-ui/button'
-import React from 'react'
-import { useHistory } from 'react-router'
-const Back = () => {
- const history = useHistory()
- const BackArrow = () => {
+import { IconButton } from "@chakra-ui/button";
+import React from "react";
+import { useHistory } from "react-router";
+const Back = ({ noBackground }) => {
+  const history = useHistory();
+  const BackArrow = () => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M7 16l-4-4m0 0l4-4m-4 4h18"
+        />
+      </svg>
+    );
+  };
   return (
-   <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-   >
-    <path
-     strokeLinecap="round"
-     strokeLinejoin="round"
-     strokeWidth="2"
-     d="M7 16l-4-4m0 0l4-4m-4 4h18"
-    />
-   </svg>
-  )
- }
- return (
-  <>
-   <IconButton
-    bg="brand.500"
-    p="2"
-    rounded="full"
-    icon={<BackArrow />}
-    color="white"
-    size="md"
-    onClick={() => history.goBack()}
-   />
-  </>
- )
-}
+    <>
+      <IconButton
+        bg={noBackground ? "brand.500" : "transparent"}
+        p="2"
+        rounded="full"
+        variant={noBackground ? "" : "outline"}
+        icon={<BackArrow />}
+        color={noBackground ? "white" : "brand.500"}
+        size="md"
+        onClick={() => history.goBack()}
+      />
+    </>
+  );
+};
 
-export default Back
+export default Back;
--- client/src/pages/TransferFundz/SearchUser.js
+++ client/src/pages/TransferFundz/SearchUser.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { Box, HStack, Text, Center, VStack } from "@chakra-ui/react";
+import { BsBank } from "react-icons/bs";
+import {
+  Box,
+  HStack,
+  Text,
+  Center,
+  VStack,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
+import { Icon } from "@chakra-ui/icon";
 import Back from "../../components/Back";
 import ProtectedComponent from "../../components/ProtectedComponent";
 import FulLogo from "../../assets/img/logo/FulLogo";
+import { Link } from "react-router-dom";
 
 const SearchUser = () => {
   return (
     <ProtectedComponent>
       {/* Go back button */}
       <HStack pl="4" pt="6" spacing="6">
-        <Back />
+        <Back noBackground={false} />
         <Text fontWeight="bold" fontSize="2xl">
           Send Money
         </Text>
@@ -17,18 +28,34 @@ const SearchUser = () => {
       {/* Go back button */}
 
       {/* Send money to user account */}
-      <HStack px="6" my="12">
+      <HStack px="4" my="12" spacing="6">
         {/* Send money to monsecure members */}
-        <VStack bg="brand.500" h="36" w="50%">
-          <Text>
-            <FulLogo showText={true} />
-          </Text>
-        </VStack>
+        <LinkBox as="section" bg="thirdColor.500" h="32" w="50%">
+          <Center h="full">
+            <VStack spacing="2">
+              <FulLogo w={14} h={14} showText={true} />
+              <Box as={Link} to="monsecure">
+                <LinkOverlay color="white" fontSize="md">
+                  To Monsecure
+                </LinkOverlay>
+              </Box>
+            </VStack>
+          </Center>
+        </LinkBox>
 
         {/* Send money to bank account*/}
-        <Box bg="thirdColor.500" h="36" w="50%">
-          {" "}
-        </Box>
+        <LinkBox as="section" bg="gray.50" h="32" w="50%">
+          <Center h="full">
+            <VStack spacing="2">
+              <Icon as={BsBank} w="12" h="12" color="brand.500" />
+              <Box as={Link} to="transfer/bank">
+                <LinkOverlay color="brand.800" fontSize="md">
+                  To Bank Account
+                </LinkOverlay>
+              </Box>
+            </VStack>
+          </Center>
+        </LinkBox>
       </HStack>
       <Box>Beneficiaries</Box>
     </ProtectedComponent>
