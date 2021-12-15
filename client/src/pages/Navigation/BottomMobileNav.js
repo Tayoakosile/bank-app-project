6df8d4427edbbfc93fa7d355770861c366b66d3f
Tayo Akosile <oluwatayoakosile@gmail.com>
2021-12-15 11:17:27 +0100

    update:creating users dashboard

--- client/src/pages/Navigation/BottomMobileNav.js
+++ client/src/pages/Navigation/BottomMobileNav.js
@@ -3,6 +3,7 @@ import { HStack, Box, Text } from "@chakra-ui/layout";
 import BottomNavigation from "reactjs-bottom-navigation";
 import "reactjs-bottom-navigation/dist/index.css";
 import useBottomNavBar from "../../hooks/useBottomNavBar";
+import ProtectedComponent from "../../components/ProtectedComponent";
 
 const BottomMobileNav = () => {
   const { ToggleBottomNavVisibility, bottomNavItems } = useBottomNavBar();
@@ -10,14 +11,16 @@ const BottomMobileNav = () => {
   return (
     <>
       {ToggleBottomNavVisibility && (
-        <Box position="fixed" zIndex="12" bottom="0" h="12" as="nav">
-          <BottomNavigation
-            items={bottomNavItems}
-            activeBgColor="#cfdced78"
-            activeTextColor="#0e3e84"
-            defaultSelected={0}
-          ></BottomNavigation>
-        </Box>
+        <ProtectedComponent>
+          <Box position="fixed" zIndex="12" bottom="0" h="12" as="nav">
+            <BottomNavigation
+              items={bottomNavItems}
+              activeBgColor=""
+              activeTextColor="#0e3e84"
+              defaultSelected={0}
+            ></BottomNavigation>
+          </Box>
+        </ProtectedComponent>
       )}
     </>
   );
